291d089bf2f2a51c071cccbfd738a4f5a99c3df0
Yasara Samaraweera <yasara@192.168.1.6>
2026-04-10 01:16:29 +0530

    add navbar with react-scroll navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-scroll";
+
+export default function Navbar() {
+    return (
+        <nav className="fixed- w-full bg-black/50 backdrop-blur z-50 p-4 flex justify-between">
+            <h1 className="text-accent font-mono">Yasara</h1>
+
+            <div className="space-x-6" hidden md:flex>
+                {["about", "skills", "projects", "contact"].map((item) => (
+                    <Link key={item} to={item} smooth className="cursor-pointer">
+                        {item}
+                    </Link>
+                ))}
+            </div>
+
+            <a href="/assets/Yasara_Samaraweera_CV_Public.pdf" className="text-accent">
+                CV
+            </a>
+        </nav>
+    );
+}
