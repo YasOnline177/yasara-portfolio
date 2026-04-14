2df6ce76cd9727fa0ff9327d1889dc37d3967f49
Yasara Samaraweera <yasara@192.168.1.2>
2026-04-14 12:19:33 +0530

    Active section highlight in navbar

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,16 +1,19 @@
 import { motion } from "framer-motion";
+import { section } from "framer-motion/client";
 
 export default function SectionWrapper({ children, id }) {
     return (
-        <motion.section 
-            id={id}
-            initial={{ opacity: 0, y: 40 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, ease: "easeOut" }}
-            viewport={{ once: true }}
-            className="max-w-6xl mx-auto px-6 py-20 relative z-10"
-        > 
-            {children}
-        </motion.section>
+        <section id={id} className="relative">
+            <motion.div
+                // id={id}
+                initial={{ opacity: 0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+                viewport={{ once: true }}
+                className="max-w-6xl mx-auto px-6 py-20 relative z-10"
+            >
+                {children}
+            </motion.div>
+        </section>
     );
 }
